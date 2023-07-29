fb56b6592165cb333fab4b9d498d108d5c614e13
becem gharbi <becem.gharbi@live.com>
2023-07-29 14:17:05 +0100

    chore: Set module file structure and imports

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,53 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addPlugin,
+  createResolver,
+  addImportsDir,
+} from "@nuxt/kit";
+import { name, version } from "../package.json";
+import { defu } from "defu";
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  httpEndpoint: string;
+  wsEndpoint?: string;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: name,
+    version: version,
+    configKey: "apollo",
+    compatibility: {
+      nuxt: "^3.0.0",
+    },
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+  // Default configuration options of the Nuxt module
+  defaults: {
+    httpEndpoint: "",
+  },
+
+  setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url);
+
+    if (options.wsEndpoint) {
+      const plugin = resolve("./runtime/plugins/apolloWithWs.ts");
+      addPlugin(plugin);
+    } else {
+      const plugin = resolve("./runtime/plugins/apolloWithoutWs.ts");
+      addPlugin(plugin);
+    }
+
+    const composables = resolve("runtime/composables");
+    addImportsDir(composables);
+
+    nuxt.options.runtimeConfig.public.apollo = defu(
+      nuxt.options.runtimeConfig.public.apollo,
+      {
+        httpEndpoint: options.httpEndpoint,
+        wsEndpoint: options.wsEndpoint,
+      }
+    );
+  },
+});
