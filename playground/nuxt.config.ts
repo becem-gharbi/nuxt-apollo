0547a30ab61aed44775a532b0c9165d3a2c9dc7b
becem gharbi <becem.gharbi@live.com>
2023-07-29 14:23:47 +0100

    feat: Add apollo client with websockets

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   apollo: {
-    wsEndpoint: "/",
+    httpEndpoint:
+      "http://127.0.0.1:8055/graphql?access_token=pT9Jm6MG5ajHjH1VQFtXLJYI1-8hsTO5",
+    wsEndpoint:
+      "ws://127.0.0.1:8055/graphql?access_token=pT9Jm6MG5ajHjH1VQFtXLJYI1-8hsTO5",
   },
   devtools: { enabled: true },
 });
--- src/runtime/plugins/apolloWithWs.ts
+++ src/runtime/plugins/apolloWithWs.ts
@@ -1,5 +1,42 @@
-import { defineNuxtPlugin } from "#imports";
+import { defineNuxtPlugin, useRuntimeConfig } from "#imports";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { createClient } from "graphql-ws";
+import { HttpLink, split } from "@apollo/client/core";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import ws from "ws";
 
-export default defineNuxtPlugin(() => {
-  console.log("with ws");
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig().public.apollo;
+
+  const httpLink = new HttpLink({
+    uri: config.httpEndpoint,
+  });
+
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: config.wsEndpoint,
+      webSocketImpl: process.server ? ws : WebSocket,
+    })
+  );
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  );
+
+  const apolloClient = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+  });
+
+  nuxtApp.vueApp.provide(DefaultApolloClient, apolloClient);
 });
